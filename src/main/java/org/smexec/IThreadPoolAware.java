f1dccf07fbae458bcb887bf3d2420559cd69d07e
armangal <arman@armangal.com>
2013-01-24 17:23:27 +0200

    issues/10

---
+++ src/main/java/org/smexec/IThreadPoolAware.java
@@ -0,0 +1,13 @@
+package org.smexec;
+
+/**
+ * One of the options to control the destination thread pool is to implement this interface by Runnable or Callble and provide the
+ * pool name
+ */
+public interface IThreadPoolAware {
+
+    /**
+     * @return the destination pool name.
+     */
+    String getPoolName();
+}
---
+++ src/test/java/org/smexec/run/SleepingThreadPoolAware.java
@@ -0,0 +1,32 @@
+package org.smexec.run;
+
+import org.smexec.IThreadPoolAware;
+
+public class SleepingThreadPoolAware
+    implements Runnable, IThreadPoolAware {
+
+    long delay;
+
+    public SleepingThreadPoolAware(long delay) {
+        super();
+        this.delay = delay;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("SleepingThreadPoolAware, Sleeping with:" + delay + "[" + Thread.currentThread().getName() + "]");
+        if (delay % 10 == 0) {
+            throw new RuntimeException("Just fail");
+        }
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getPoolName() {
+        return "Custom1";
+    }
+}
